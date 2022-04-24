334d92ce14dc4b4d05ef15cec39f824f4ba3318d
narumi <yan.jianhui147@gmail.com>
2022-04-25 00:09:09 +0800

    add Arabic  translation from SORA

--- lib/generated/intl/messages_ar.dart
+++ lib/generated/intl/messages_ar.dart
@@ -20,9 +20,166 @@ typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
 class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'ar';
 
+  static String m0(a, b) => "${a} ${b}";
+
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "about_app": MessageLookupByLibrary.simpleMessage("عن التطبيق"),
+        "about_app_declaration_text": MessageLookupByLibrary.simpleMessage(
+            "البيانات ضمن هذا التطبيق مصدرها لعبة Fate/GO والمواقع الاكترونية التابعة. حقوق نشر النصوص الأصلية، الصور، والصوتيات تنتمي الى TYPE MOON/FGO PROJECT.\n\n تصميم التطبيق مبني على التطبيق المصغر ل وي شات \"material programe\" وتطبيق ال آي او اس \"GUDA\". \n"),
+        "about_data_source":
+            MessageLookupByLibrary.simpleMessage("مصدر البيانات"),
+        "about_data_source_footer": MessageLookupByLibrary.simpleMessage(
+            "الرجاء إعلامنا في حالة وجود مصادر غير مذكورة او تم التعدي عليها."),
+        "about_feedback": MessageLookupByLibrary.simpleMessage("المرجعية "),
+        "active_skill": MessageLookupByLibrary.simpleMessage("المهارة النشطة"),
+        "add": MessageLookupByLibrary.simpleMessage("أضف "),
+        "add_to_blacklist":
+            MessageLookupByLibrary.simpleMessage("ضف الى اللائحة السوداء"),
+        "ap": MessageLookupByLibrary.simpleMessage("AP"),
+        "ap_efficiency": MessageLookupByLibrary.simpleMessage("AP نسبة"),
+        "append_skill": MessageLookupByLibrary.simpleMessage(" الباسيڨ سكيل"),
+        "append_skill_short":
+            MessageLookupByLibrary.simpleMessage("باسيف سكيل"),
+        "ascension": MessageLookupByLibrary.simpleMessage("أسينشن "),
+        "ascension_short": MessageLookupByLibrary.simpleMessage("أسينشن "),
+        "ascension_up": MessageLookupByLibrary.simpleMessage("تصعيد"),
+        "attachment": MessageLookupByLibrary.simpleMessage("مرفق"),
+        "auto_reset": MessageLookupByLibrary.simpleMessage("إعادة ضبط تلقائي "),
+        "auto_update": MessageLookupByLibrary.simpleMessage("تحديث تلقائي"),
+        "backup": MessageLookupByLibrary.simpleMessage("النسخ احتياطي "),
+        "backup_history":
+            MessageLookupByLibrary.simpleMessage("تاريخ النسخ الاحتياطي "),
+        "blacklist": MessageLookupByLibrary.simpleMessage("القائمة السوداء "),
+        "bond": MessageLookupByLibrary.simpleMessage("البوند"),
+        "bond_craft": MessageLookupByLibrary.simpleMessage("كرافت البوند"),
+        "bond_eff": MessageLookupByLibrary.simpleMessage("تأثير البوند"),
+        "bronze": MessageLookupByLibrary.simpleMessage("برونز"),
+        "calc_weight": MessageLookupByLibrary.simpleMessage("وزن"),
+        "cancel": MessageLookupByLibrary.simpleMessage("إلغاء"),
+        "card_description": MessageLookupByLibrary.simpleMessage("الوصف"),
+        "card_info": MessageLookupByLibrary.simpleMessage("عن"),
+        "carousel_setting":
+            MessageLookupByLibrary.simpleMessage("اعدادات المكتبة"),
+        "change_log": MessageLookupByLibrary.simpleMessage("سجل التغييرات "),
+        "characters_in_card": MessageLookupByLibrary.simpleMessage("حروف"),
+        "check_update": MessageLookupByLibrary.simpleMessage("تفقد عن تحديث "),
+        "clear": MessageLookupByLibrary.simpleMessage("مسح"),
+        "clear_cache":
+            MessageLookupByLibrary.simpleMessage("مسح ذاكرة التخزين المؤقت"),
+        "clear_cache_finish": MessageLookupByLibrary.simpleMessage(
+            "ذاكرة التخزين المؤقت تم مسحها"),
+        "clear_cache_hint":
+            MessageLookupByLibrary.simpleMessage("يشمل الصور والصوتيات"),
+        "command_code": MessageLookupByLibrary.simpleMessage("كوماند كود"),
+        "confirm": MessageLookupByLibrary.simpleMessage("تأكيد"),
+        "consumed": MessageLookupByLibrary.simpleMessage("المستهلك"),
+        "copied": MessageLookupByLibrary.simpleMessage("منسوخ"),
+        "copy": MessageLookupByLibrary.simpleMessage("نسخ"),
+        "copy_plan_menu": MessageLookupByLibrary.simpleMessage("نسخ خطة من…"),
+        "costume": MessageLookupByLibrary.simpleMessage("زي خاص"),
+        "costume_unlock": MessageLookupByLibrary.simpleMessage("فتح زي خاص"),
+        "counts": MessageLookupByLibrary.simpleMessage("Counts"),
+        "craft_essence": MessageLookupByLibrary.simpleMessage("الكرافتز"),
+        "cur_account": MessageLookupByLibrary.simpleMessage("الحساب الحالي"),
+        "current_": MessageLookupByLibrary.simpleMessage("حالي "),
+        "dark_mode": MessageLookupByLibrary.simpleMessage("الوضع المظلم"),
+        "dark_mode_dark": MessageLookupByLibrary.simpleMessage("المظلم"),
+        "dark_mode_light": MessageLookupByLibrary.simpleMessage("الوضع المضيئ"),
+        "dark_mode_system": MessageLookupByLibrary.simpleMessage("حسب النظام"),
+        "delete": MessageLookupByLibrary.simpleMessage("حذف"),
+        "demands": MessageLookupByLibrary.simpleMessage("متطلبات"),
+        "display_setting":
+            MessageLookupByLibrary.simpleMessage("إعدادات العرض"),
+        "download": MessageLookupByLibrary.simpleMessage("تحميل "),
+        "download_latest_gamedata_hint": MessageLookupByLibrary.simpleMessage(
+            "لضمان التوافق الرجاء تحديث آخر نسخة من التطبيق قبل تحديث البيانات "),
+        "download_source": MessageLookupByLibrary.simpleMessage("مصدر التحميل"),
+        "download_source_hint": MessageLookupByLibrary.simpleMessage(
+            "تحديث حزمة البيانات والتطبيق"),
+        "downloaded": MessageLookupByLibrary.simpleMessage("تم التحميل"),
+        "downloading": MessageLookupByLibrary.simpleMessage("جاري التحميل"),
+        "drop_calc_empty_hint":
+            MessageLookupByLibrary.simpleMessage("اضغط + لإضافة عنصر"),
+        "drop_calc_min_ap": MessageLookupByLibrary.simpleMessage("Min AP"),
+        "drop_calc_solve": MessageLookupByLibrary.simpleMessage("احسب"),
+        "drop_rate": MessageLookupByLibrary.simpleMessage("نسبة الدروب"),
+        "edit": MessageLookupByLibrary.simpleMessage("تعديل "),
+        "effect_search": MessageLookupByLibrary.simpleMessage("البحث عن بفف"),
+        "efficiency_type_drop":
+            MessageLookupByLibrary.simpleMessage("نسبة الدروب"),
+        "enemy_list": MessageLookupByLibrary.simpleMessage("الاعداء"),
         "language": MessageLookupByLibrary.simpleMessage("عربي"),
-        "language_en": MessageLookupByLibrary.simpleMessage("Arabic")
+        "language_en": MessageLookupByLibrary.simpleMessage("Arabic"),
+        "restore": MessageLookupByLibrary.simpleMessage("إعادة"),
+        "save": MessageLookupByLibrary.simpleMessage("حفط"),
+        "save_to_photos":
+            MessageLookupByLibrary.simpleMessage("الحفظ في الصور"),
+        "saved": MessageLookupByLibrary.simpleMessage("محفوظ "),
+        "search": MessageLookupByLibrary.simpleMessage("بحث"),
+        "search_option_basic": MessageLookupByLibrary.simpleMessage("اساسي"),
+        "select_copy_plan_source":
+            MessageLookupByLibrary.simpleMessage("تحديد مصدر النسخ"),
+        "select_plan": MessageLookupByLibrary.simpleMessage("تحديد خطة"),
+        "servant": MessageLookupByLibrary.simpleMessage("الخادم"),
+        "servant_coin": MessageLookupByLibrary.simpleMessage("عملة الخدم"),
+        "servant_title": MessageLookupByLibrary.simpleMessage("الخادم"),
+        "set_plan_name":
+            MessageLookupByLibrary.simpleMessage("اعداد اسم الخطة"),
+        "setting_auto_rotate":
+            MessageLookupByLibrary.simpleMessage("تدوير تلقائي"),
+        "settings_data": MessageLookupByLibrary.simpleMessage("بيانات"),
+        "settings_documents": MessageLookupByLibrary.simpleMessage("المستندات"),
+        "settings_general": MessageLookupByLibrary.simpleMessage("عام"),
+        "settings_language": MessageLookupByLibrary.simpleMessage("اللغة"),
+        "settings_tab_name": MessageLookupByLibrary.simpleMessage("الإعدادات"),
+        "share": MessageLookupByLibrary.simpleMessage("مشاركة"),
+        "show_outdated":
+            MessageLookupByLibrary.simpleMessage("عرض المنتهى منه"),
+        "silver": MessageLookupByLibrary.simpleMessage("فضي "),
+        "simulator": MessageLookupByLibrary.simpleMessage("المحاكي"),
+        "skill": MessageLookupByLibrary.simpleMessage("مهارة"),
+        "skill_up": MessageLookupByLibrary.simpleMessage("رفع المهارة"),
+        "skilled_max10":
+            MessageLookupByLibrary.simpleMessage("المهارات القصوى (310)"),
+        "sprites": MessageLookupByLibrary.simpleMessage("الأرواح"),
+        "statistics_title": MessageLookupByLibrary.simpleMessage("احصائيات"),
+        "success": MessageLookupByLibrary.simpleMessage("نجاح"),
+        "summon": MessageLookupByLibrary.simpleMessage("استدعاء"),
+        "summon_title": MessageLookupByLibrary.simpleMessage("الاستدعاءات"),
+        "support_chaldea":
+            MessageLookupByLibrary.simpleMessage("الدعم والتبرعات"),
+        "svt_not_planned": MessageLookupByLibrary.simpleMessage("غير مفضل"),
+        "svt_plan_hidden": MessageLookupByLibrary.simpleMessage("مخفي"),
+        "svt_reset_plan":
+            MessageLookupByLibrary.simpleMessage("إعادة ضبط الخطة"),
+        "svt_switch_slider_dropdown":
+            MessageLookupByLibrary.simpleMessage(" تبديل الشرائح /المهابط"),
+        "tooltip_refresh_sliders":
+            MessageLookupByLibrary.simpleMessage("تحديث الشرائح"),
+        "total_ap": MessageLookupByLibrary.simpleMessage("الاي بي الكلي"),
+        "total_counts": MessageLookupByLibrary.simpleMessage("الحساب النهائي"),
+        "update": MessageLookupByLibrary.simpleMessage("تحديث "),
+        "update_already_latest":
+            MessageLookupByLibrary.simpleMessage("النسخة الاعلي مسبقا"),
+        "update_dataset":
+            MessageLookupByLibrary.simpleMessage("تحديث حزمة البيانات "),
+        "upload": MessageLookupByLibrary.simpleMessage("رفع"),
+        "userdata": MessageLookupByLibrary.simpleMessage("بيانات المستخدم "),
+        "userdata_download_backup":
+            MessageLookupByLibrary.simpleMessage("تحميل نسخة احتياطية"),
+        "userdata_download_choose_backup":
+            MessageLookupByLibrary.simpleMessage("اختر نسخة احتياطية "),
+        "userdata_sync":
+            MessageLookupByLibrary.simpleMessage("مزامنة البيانات"),
+        "userdata_upload_backup":
+            MessageLookupByLibrary.simpleMessage("رفع نسخة احتياطية "),
+        "valentine_craft":
+            MessageLookupByLibrary.simpleMessage("كرافت عيد الحب"),
+        "version": MessageLookupByLibrary.simpleMessage("النسخة"),
+        "view_illustration":
+            MessageLookupByLibrary.simpleMessage("عرض الرسومية"),
+        "voice": MessageLookupByLibrary.simpleMessage("صوت"),
+        "words_separate": m0
       };
 }
