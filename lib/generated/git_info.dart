f127a55b803163bc19b2bf4762ecf9655dd04056
narumi <yan.jianhui147@gmail.com>
2022-05-07 21:29:53 +0800

    Bump version to 1.8.6+860

--- lib/generated/git_info.dart
+++ lib/generated/git_info.dart
@@ -1,3 +1,3 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
-const String kCommitHash = "572763";
-const int kCommitTimestamp = 1651631815;
+const String kCommitHash = "c74ab6";
+const int kCommitTimestamp = 1651930149;
