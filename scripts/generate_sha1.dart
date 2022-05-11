966a8858137cc10fee3a2a4297f5383f9f823f6f
narumi <yan.jianhui147@gmail.com>
2022-05-11 20:21:31 +0800

    update github release workflow

---
+++ scripts/generate_sha1.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+import 'package:crypto/crypto.dart';
+
+void main(List<String> args) {
+  final fp = args.first;
+  final contents = File(fp).readAsBytesSync();
+  final checksum = sha1.convert(contents).toString().toLowerCase();
+  File(fp + '.sha1').writeAsStringSync(checksum);
+  print('File: $fp');
+  print('SHA1: $checksum');
+}
