5a07f62500659f925f6b8dfa66d6b9461847b9ae
narumi <yan.jianhui147@gmail.com>
2022-06-06 15:51:09 +0800

    retry opening hive box

--- lib/utils/hive_extention.dart
+++ lib/utils/hive_extention.dart
@@ -17,32 +17,33 @@ extension HiveRetryOpen on HiveInterface {
     bool crashRecovery = true,
     String? path,
     Uint8List? bytes,
+    int retry = 3,
   }) async {
-    Box<E> box;
-    try {
-      box = await openBox<E>(
-        name,
-        encryptionCipher: encryptionCipher,
-        keyComparator: keyComparator,
-        compactionStrategy: compactionStrategy,
-        crashRecovery: crashRecovery,
-        path: path,
-        bytes: bytes,
-      );
-    } catch (e, s) {
-      logger.e('open Box<$E> "$name" failed', e, s);
-      await deleteBoxFromDisk(name, path: path);
-      box = await openBox<E>(
-        name,
-        encryptionCipher: encryptionCipher,
-        keyComparator: keyComparator,
-        compactionStrategy: compactionStrategy,
-        crashRecovery: crashRecovery,
-        path: path,
-        bytes: bytes,
-      );
+    Box<E>? box;
+    int n = 0;
+    while (n < retry) {
+      n += 1;
+      try {
+        box = await openBox<E>(
+          name,
+          encryptionCipher: encryptionCipher,
+          keyComparator: keyComparator,
+          compactionStrategy: compactionStrategy,
+          crashRecovery: crashRecovery,
+          path: path,
+          bytes: bytes,
+        );
+        return box;
+      } catch (e, s) {
+        logger.e('open Box<$E> "$name" failed', e, s);
+        await Future.delayed(const Duration(seconds: 1));
+        if (n % 2 == 0) {
+          await deleteBoxFromDisk(name, path: path);
+        }
+        if (n >= retry) rethrow;
+      }
     }
-    return box;
+    throw HiveError('Failed to open hive box: $name');
   }
 
   Future<LazyBox<E>> openLazyBoxRetry<E>(
