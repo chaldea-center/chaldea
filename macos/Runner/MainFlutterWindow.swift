ca6252169f0bc6814d87835f37bff87591387e9c
narumi <yan.jianhui147@gmail.com>
2022-10-14 05:20:29 +0800

    Enable mac app fullscreen

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -7,6 +7,7 @@ class MainFlutterWindow: NSWindow {
     let windowFrame = self.frame
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
+    self.collectionBehavior = .fullScreenPrimary
 
     RegisterGeneratedPlugins(registry: flutterViewController)
     configMethodChannel()
