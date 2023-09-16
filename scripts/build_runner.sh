3d45b20d6476e1925fcd977a90b5f210fe7016fa
narumi <yan.jianhui147@gmail.com>
2023-09-16 18:55:54 +0800

    Join commands for shell scripts

--- scripts/build_runner.sh
+++ scripts/build_runner.sh
@@ -1,3 +1,3 @@
 # json_serializable
-dart run build_runner build --delete-conflicting-outputs
+dart run build_runner build --delete-conflicting-outputs && \
 dart format "lib/generated/" --line-length 120
