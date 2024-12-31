16256e7346ee6ee15c05e693912bfe0dc1641662
narumi <yan.jianhui147@gmail.com>
2025-01-01 02:28:57 +0800

    Update with flutter 3.27 template

--- ios/Chaldea/AppDelegate.swift
+++ ios/Chaldea/AppDelegate.swift
@@ -2,7 +2,7 @@ import Flutter
 import UIKit
 import flutter_local_notifications
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
