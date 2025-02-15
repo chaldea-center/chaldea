1b517ee7c9670ff6e06a23861214cba7bac074f6
narumi <yan.jianhui147@gmail.com>
2025-02-15 15:08:30 +0800

    new web bootstrap

--- scripts/patch_web_build.dart
+++ scripts/patch_web_build.dart
@@ -32,61 +32,66 @@ void main() {
 
   final mainJs = _FileInfo('main.dart.js'),
       indexHtml = _FileInfo('index.html'),
+      bootstrapJs = _FileInfo('flutter_bootstrap.js'),
       // jsMap = _FileInfo('main.dart.js.map'),
       sw = _FileInfo('flutter_service_worker.js');
-
-  final flutterVersion = Platform.environment['FLUTTER_VERSION'];
-  if (flutterVersion != null && RegExp(r'').hasMatch(flutterVersion)) {
-    indexHtml.content = indexHtml.content.replaceAll('?v=FLUTTER_VERSION', '?v=$flutterVersion');
-  }
 
   // change google fonts url for cn
   print('[patch-web] patching "${mainJs.fn}"');
   int patched = 0;
-  mainJs.content = mainJs.content.replaceAllMapped(
-    RegExp(r'"https://fonts\.googleapis\.com|"https://fonts\.gstatic\.com'),
-    (m) {
-      final host = m.group(0)!;
-      if (host == '"https://fonts.googleapis.com') {
-        patched++;
-        return '(window.isCNHost?"https://fonts.font.im":"https://fonts.googleapis.com")+"';
-      } else if (host == '"https://fonts.gstatic.com') {
-        patched++;
-        return '(window.isCNHost?"https://fonts.gstatic.font.im":"https://fonts.gstatic.com")+"';
-      } else {
-        throw 'unknown host: <$host>';
-      }
-    },
-  );
+  // mainJs.content = mainJs.content.replaceAllMapped(
+  //   RegExp(r'"https://fonts\.googleapis\.com|"https://fonts\.gstatic\.com'),
+  //   (m) {
+  //     final host = m.group(0)!;
+  //     if (host == '"https://fonts.googleapis.com') {
+  //       patched++;
+  //       return '(window.isCNHost?"https://fonts.font.im":"https://fonts.googleapis.com")+"';
+  //     } else if (host == '"https://fonts.gstatic.com') {
+  //       patched++;
+  //       return '(window.isCNHost?"https://fonts.gstatic.font.im":"https://fonts.gstatic.com")+"';
+  //     } else {
+  //       throw 'unknown host: <$host>';
+  //     }
+  //   },
+  // );
   mainJs.updateHash();
   print('[patch-web] patched $patched google fonts code lines.');
 
   // replace all main.dart.js reference to new hashed filename
-  final mainjsHash = mainJs.newHash.substring(0, 8);
+  bootstrapJs.content = bootstrapJs.content.replaceAll('MAIN_DART_JS_VERSION', mainJs.newHash.substring(0, 8));
+  bootstrapJs.updateHash();
 
-  indexHtml.content = indexHtml.content.replaceFirst('main.dart.js?v=VERSION', 'main.dart.js?v=$mainjsHash');
+  indexHtml.content = indexHtml.content.replaceAll('BOOTSTRAP_VERSION', bootstrapJs.newHash.substring(0, 8));
   indexHtml.updateHash();
 
   // remove NOTICE from core cache, which needs to download before app start
   sw.content = sw.content.replaceFirst('"assets/NOTICES",\n', '');
 
   // update all cache hash
+  final filesNeedUpdateHash = <String, _FileInfo>{
+    "/": indexHtml,
+    for (final f in [indexHtml, mainJs, bootstrapJs]) f.fn: f,
+  };
   sw.content = sw.content.replaceAllMapped(RegExp(r'"([^"]+)":\s*"([0-9a-f]{32})"'), (match) {
     final fn = match.group(1)!;
     final oldHash = match.group(2)!;
-    if (fn == 'index.html' || fn == '/') {
-      print('Updating hash "${fn.padRight(21)}": $oldHash -> ${indexHtml.newHash}');
-      assert(oldHash == indexHtml.initHash);
-      return '"$fn": "${indexHtml.newHash}"';
-    } else if (fn == mainJs.fn) {
-      print('Updating hash "${fn.padRight(21)}": $oldHash -> ${mainJs.newHash}');
-      assert(oldHash == mainJs.initHash);
-      return '"$fn": "${mainJs.newHash}"';
+
+    final fileinfo = filesNeedUpdateHash[fn];
+    if (fileinfo != null) {
+      if (oldHash == fileinfo.newHash) {
+        print('"${fn.padRight(21)}": updating hash $oldHash -> unchanged');
+      } else {
+        print('"${fn.padRight(21)}": updating hash $oldHash -> ${fileinfo.newHash}');
+        return '"$fn": "${fileinfo.newHash}"';
+      }
+      assert(oldHash == fileinfo.initHash);
     }
+
     return match.group(0)!;
   });
 
   indexHtml.save();
+  bootstrapJs.save();
   mainJs.save();
   // jsMap.save();
   sw.save();
---
+++ web/flutter_bootstrap.js
@@ -0,0 +1,27 @@
+(function(){
+var self=this||{};try{self.WeakRef=WeakRef,self.FinalizationGroup=FinalizationGroup}catch(e){!function(e,t){var i=new e;function n(e){i.set(this,e)}t(n.prototype,{deref:{value:function(){return i.get(this)}}});var a=new e;function r(e){a.set(this,[])}t(r.prototype,{register:{value:function(e,t){var i=a.get(this);i.indexOf(t)<0&&i.push(t)}},unregister:{value:function(e,t){var i=a.get(this),n=i.indexOf(t);return-1<n&&i.splice(n,1),-1<n}},cleanupSome:{value:function(e){e(a.get(this))}}}),self.WeakRef=n,self.FinalizationGroup=r}(WeakMap,Object.defineProperties)}
+})();
+
+{{flutter_js}}
+{{flutter_build_config}}
+
+_flutter.buildConfig.builds[0].mainJsPath = "/main.dart.js?v=MAIN_DART_JS_VERSION";
+
+let _host = window.location.hostname.toLowerCase();
+window.isCNHost =
+  "true" === window.localStorage.getItem("useProxy") ||
+  _host.startsWith("cn.") ||
+  -1 != "narumi.cc".indexOf(_host) ||
+  -1 != ["localhost"].indexOf(_host);
+
+const userConfig = {
+  canvasKitBaseUrl: "/canvaskit/",
+  renderer: "canvaskit",
+  fontFallbackBaseUrl: window.isCNHost
+    ? "https://fonts.gstatic.font.im/s/"
+    : null,
+};
+
+_flutter.loader.load({
+  config: userConfig,
+});
