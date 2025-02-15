43db88506f677ca57941611c06fb6575befba324
narumi <yan.jianhui147@gmail.com>
2025-02-15 22:37:30 +0800

    Fix flutter 3.29
    - android: remove v1 embedding
    - android: migrate to .gradle.kts
    - upgrade cocoapods

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,96 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.konan.properties.Properties
+import java.io.FileInputStream
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+val keystorePropertiesFile: File = rootProject.file("key.properties")
+val keystoreProperties = Properties()
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
+android {
+    namespace = "cc.narumi.chaldea"
+    compileSdk = 35
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    flavorDimensions += "app"
+    productFlavors {
+        create("play") {
+            dimension = "app"
+            isDefault = true
+        }
+        create("fdroid") {
+            dimension = "app"
+            applicationIdSuffix = ".fdroid"
+        }
+    }
+
+    defaultConfig {
+        applicationId = "cc.narumi.chaldea"
+        minSdk = 23
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+        multiDexEnabled = true
+
+        resValue("string", "app_name", "Chaldea")
+    }
+
+    signingConfigs {
+        if (keystorePropertiesFile.exists()) {
+            register("release") {
+                keyAlias = keystoreProperties.getProperty("keyAlias")
+                keyPassword = keystoreProperties.getProperty("keyPassword")
+                storeFile = file(keystoreProperties.getProperty("storeFile"))
+                storePassword = keystoreProperties.getProperty("storePassword")
+            }
+        }
+    }
+
+    buildTypes {
+        all {
+            signingConfig = signingConfigs.getByName("release")
+        }
+        release {
+            ndk {
+                debugSymbolLevel = "FULL"
+            }
+        }
+        // create("profile") {
+        //     applicationIdSuffix = ".profile"
+        //     resValue("string", "app_name", "Chaldea Profile")
+        // }
+        debug {
+            applicationIdSuffix = ".debug"
+            resValue("string", "app_name", "Chaldea Debug")
+        }
+    }
+    lint {
+        checkReleaseBuilds = false
+    }
+}
+
+flutter {
+    source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+}
---
+++ android/build.gradle.kts
@@ -0,0 +1,24 @@
+allprojects {
+    repositories {
+        // maven{ url 'https://maven.aliyun.com/repository/central' }
+        // maven{ url 'https://maven.aliyun.com/repository/public' }
+        // maven{ url 'https://maven.aliyun.com/repository/gradle-plugin'}
+        google()
+        mavenCentral()
+    }
+}
+
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
