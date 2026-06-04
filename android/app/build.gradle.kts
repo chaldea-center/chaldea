01e7f138a5ebc1367ac77e167907dfabbab92a95
narumi <yan.jianhui147@gmail.com>
2026-06-05 01:03:19 +0800

    Revert "Improve android widget auto update"
    This reverts commit fa0908b890787e758ba47e1a8574435966a85a43.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -102,12 +102,6 @@ flutter {
     source = "../.."
 }
 
-configurations.all {
-    resolutionStrategy {
-        force("androidx.glance:glance-appwidget:1.1.1")
-    }
-}
-
 dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
     implementation("androidx.glance:glance-appwidget:1.1.1")
